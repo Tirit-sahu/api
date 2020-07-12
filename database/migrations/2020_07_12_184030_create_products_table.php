2bb65525f9caa99415042f2b74d8fe27be533d2d
Tirit sahu <tirithesahu.com>
2020-07-13 00:43:57 +0530

    Migration created for product and review

--- database/migrations/2020_07_12_184030_create_products_table.php
+++ database/migrations/2020_07_12_184030_create_products_table.php
@@ -14,7 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->text('details');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
