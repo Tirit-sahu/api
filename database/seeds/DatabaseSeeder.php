2fbf88927d8fa5c11213d13766a2ed3f0f994b8f
Tirit sahu <tirithesahu.com>
2020-07-13 01:42:19 +0530

    Relationship created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         factory(App\Model\Product::class,50)->create();
-        factory(App\Model\Review::class,50)->create();
+        factory(App\Model\Review::class,300)->create();
     }
 }
