3a98dc9f79a95ca78136199909c3f50e91818336
Tirit sahu <tirithesahu.com>
2020-07-17 22:59:44 +0530

    Product Update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    protected $fillable = [
+        'name','details','stock','price','discount'
+    ];
+
+
     public function reviews()
     {
     	return $this->hasMany(Review::class);
