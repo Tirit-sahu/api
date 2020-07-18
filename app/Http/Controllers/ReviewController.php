f7541a85a301bee6cacb5eb45a2f0f3002e70782
Tirit sahu <tirithesahu.com>
2020-07-19 00:18:11 +0530

    Api all parts completed

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -43,7 +43,7 @@ class ReviewController extends Controller
         $product->reviews()->save($review);
         return response([
             'data' => new ReviewResource($review)
-        ],Response::HTTP_ACCEPTED);
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -75,9 +75,12 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Product $product, Review $review)
     {
-        //
+        $review->update($request->all());
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -86,8 +89,9 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+        $review->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
